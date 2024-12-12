5421a64edff16697784d076d48bc77c6795b478c
prince raj <dev.prince636@gmail.com>
2024-12-13 01:45:25 +0530

    build issue fixed

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -3,19 +3,23 @@ import authImg from "../../assets/images/auth2.jpg";
 import logoCodex from "../../assets/images/codexWhite.svg";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
-import { Button } from "../common/Button";
+import { Link, useNavigate } from "react-router-dom";
 import { authEndpoints } from "../../services/apiEndpoints";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
 
-export const Login = () => {
-  //  get login url;
-  const { LOGIN_URL } = authEndpoints;
+export const Signup = () => {
+  //  get url;
+  const { SIGNUP_URL } = authEndpoints;
   const navigate = useNavigate();
   const [showPass, setShowPass] = useState(false);
+  const [showCnfPass, setShowCnfPass] = useState(false);
+  // managing state using react forms;
   const [formData, setFormData] = useState({
+    name: "",
+    username: "",
     email: "",
     password: "",
+    confirmPass: "",
   });
 
   //  form data change handler
@@ -28,41 +32,39 @@ export const Login = () => {
 
   //  form submit handler;
   const submitHandler = async (e) => {
-    e.preventDefault();
     const toastId = toast.loading("Loading...");
+    e.preventDefault();
+    if (formData.password !== formData.confirmPass) {
+      console.log("pass not matched");
+      return toast.error("Password do not match");
+    }
+
+    //  make an api call;
     try {
-      // make an api call;
-      const response = await fetch(LOGIN_URL, {
+      const response = await fetch(SIGNUP_URL, {
         mode: "cors",
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
+          name: formData.name,
+          userName: formData.username.trim(),
           email: formData.email.toLowerCase().trim(),
           password: formData.password.trim(),
         }),
       });
 
-      //  convert data into json format;
-      // console.log(response);
+      // convert response into json format;
       const result = await response.json();
-
-      console.log(result);
-      if (result?.success) {
-        const token = result?.data?.token;
-        const id = result?.data.user?._id;
-        const user = result?.data?.user;
-        localStorage.setItem("token", token);
-        localStorage.setItem("userId", id);
-        localStorage.setItem("user", user);
-        localStorage.setItem("isLoggedIn", true);
-        toast.success(result.message);
-        navigate("/");
-      } else {
+      if (!result.success) {
         toast.error(result.message);
         throw Error("Something went wrong");
+      } else {
+        toast.success(result.message);
+        navigate("/login");
       }
+      // console.log(result);
     } catch (err) {
       console.log(err);
     }
@@ -75,8 +77,30 @@ export const Login = () => {
         <div className="left w-[35%] ">
           <img className="w-[150px]" src={logoCodex} alt="codexLogo" />
 
-          {/*  Login form */}
+          {/*  signUp form */}
           <form onSubmit={submitHandler} className="w-full mt-[60px]">
+            <div className="inputBox">
+              <input
+                required
+                onChange={changeHandler}
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={formData.name}
+              />
+            </div>
+
+            <div className="inputBox">
+              <input
+                required
+                onChange={changeHandler}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={formData.username}
+              />
+            </div>
+
             <div className="inputBox">
               <input
                 required
@@ -107,9 +131,35 @@ export const Login = () => {
               </div>
             </div>
 
+            <div className="inputBox flex justify-between items-center">
+              <input
+                type={showCnfPass ? "text" : "password"}
+                required
+                onChange={changeHandler}
+                placeholder="Confirm Password"
+                name="confirmPass"
+                value={formData.confirmPass}
+              />
+
+              <div onClick={() => setShowCnfPass(!showCnfPass)}>
+                {showCnfPass ? (
+                  <IoEyeOff size={22} fill={"#77cffc"} />
+                ) : (
+                  <IoEye size={22} fill={"red"} />
+                )}
+              </div>
+            </div>
+
+            <p className="text-[gray] text-lg text-right">
+              Already have an account?{" "}
+              <Link to="/login" className="text-[#00AEEF]">
+                login
+              </Link>
+            </p>
+
             {/* submit button */}
             <button className={`btn-grad mt-[20px] text-xl w-full`}>
-              Login
+              Sign Up
             </button>
           </form>
         </div>
